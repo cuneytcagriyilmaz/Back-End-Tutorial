f309561a536b7b76a3381e8ef4cc1d3e575853fb
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-03-16 11:26:59 +0300

    Add files via upload

---
+++ Uygulamalar/Ciftdordunkati.java
@@ -0,0 +1,25 @@
+package Uygulamalar;
+
+import java.util.Scanner;
+
+
+
+public class Ciftdordunkati {
+    public static void main(String[] args) {
+
+        int n;
+        int total = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı Giriniz: ");
+            n = input.nextInt();
+            if (n % 4 == 0) {
+                total += n;
+
+            }
+        } while (n % 2 == 0);
+        System.out.print("Total " + total);
+    }
+}
