26f0f775b526911bdbb531ede211cd1ae288daa7
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-03-22 00:27:07 +0300

    Add files via upload

---
+++ Uygulamalar/Tersucgen.java
@@ -0,0 +1,21 @@
+package Uygulamalar;
+import java.util.Scanner;
+public class Tersucgen {
+    public static void main(String[] args) {
+        int sayi;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Basamk sayısını giriniz:");
+        sayi = sc.nextInt();
+        int a;
+        for ( a = 1; a < sayi; a++) {
+            for (int b = 1; b <= a; b++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= 2 * (sayi - a) - 1; c++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+    }
+}
