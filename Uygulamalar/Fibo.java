a684cdb285c85fd6458dfafd007205ce2e53d172
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-03-22 00:33:17 +0300

    Add files via upload

---
+++ Uygulamalar/Fibo.java
@@ -0,0 +1,15 @@
+package Uygulamalar;
+import java.util.Scanner;
+public class Fibo {
+    public static void main(String[] args) {
+        int a = 1, b = 1, before = 1, sum = 0;
+        System.out.print(a + " " + b + " ");
+        for (int i = 0; i < 12; i++) {
+            before = a;
+            sum = before + b;
+            a = sum;
+            b = before;
+            System.out.print(sum + " ");
+        }
+    }
+}
