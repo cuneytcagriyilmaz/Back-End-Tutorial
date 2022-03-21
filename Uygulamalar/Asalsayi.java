a9354a0476bf96ec1b344829d6766875201df637
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-03-22 00:29:25 +0300

    Add files via upload

---
+++ Uygulamalar/Asalsayi.java
@@ -0,0 +1,20 @@
+package Uygulamalar;
+
+import java.util.Scanner;
+
+public class Asalsayi {
+    public static void main(String[] args) {
+        for (int i = 2; i < 100; i++) {
+            boolean istrue = true;
+            for (int a = 2; a < i; a++) {
+                if (i % a == 0) {
+                    istrue = false;
+                    break;
+                }
+            }
+            if (istrue) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
