102b520baa248c3046d47b99ef889bc5d12c1ceb
cuneytcagriyilmaz <88775443+cuneytcagriyilmaz@users.noreply.github.com>
2022-04-12 19:24:26 +0300

    Create sql8.sql

---
+++ SQL/sql8.sql
@@ -0,0 +1,149 @@
+-- 1)test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+
+--CREATE TABLE test(
+--	id INTEGER NOT NULL,
+--	name VARCHAR(50) NOT NULL,
+--	birthday DATE NOT NULL,
+--	email VARCHAR(100)
+--)
+
+
+insert into test (id, name, birthday, email) values (2, 'Lillis', '5/6/1986', 'ltaylot1@nifty.com');
+insert into test (id, name, birthday, email) values (4, 'Patin', '3/14/1970', 'ppaur3@people.com.cn');
+insert into test (id, name, birthday, email) values (5, 'Lorine', '7/20/1936', 'lgebbe4@theglobeandmail.com');
+insert into test (id, name, birthday, email) values (6, 'Candida', '8/27/1966', 'cschenkel5@netscape.com');
+insert into test (id, name, birthday, email) values (7, 'Carlene', '5/3/2001', 'ckennet6@patch.com');
+insert into test (id, name, birthday, email) values (8, 'Shermy', '5/28/1998', 'sberriman7@google.com.br');
+insert into test (id, name, birthday, email) values (9, 'Mechelle', '8/12/2004', 'msly8@slashdot.org');
+insert into test (id, name, birthday, email) values (10, 'Deane', '10/1/1977', 'dquan9@infoseek.co.jp');
+insert into test (id, name, birthday, email) values (11, 'Anjanette', '5/25/1945', 'aadriania@usatoday.com');
+insert into test (id, name, birthday, email) values (12, 'Del', '2/17/1987', 'dgarbuttb@ibm.com');
+insert into test (id, name, birthday, email) values (13, 'Davin', '1/12/1935', 'dabrahamowitczc@wp.com');
+insert into test (id, name, birthday, email) values (14, 'Nara', '11/19/1978', 'nmcelweed@omniture.com');
+insert into test (id, name, birthday, email) values (15, 'Ronalda', '11/5/1915', 'rhollyare@oracle.com');
+insert into test (id, name, birthday, email) values (16, 'Viva', '3/6/1975', 'vpoulsenf@mail.ru');
+insert into test (id, name, birthday, email) values (17, 'Elianore', '10/20/1971', 'ekornyakovg@sfgate.com');
+insert into test (id, name, birthday, email) values (18, 'Camila', '6/11/1894', 'cferfulleh@freewebs.com');
+insert into test (id, name, birthday, email) values (19, 'Kayle', '8/18/1918', 'kklementzi@com.com');
+insert into test (id, name, birthday, email) values (20, 'Fredric', '6/17/2009', 'fwashbrookj@angelfire.com');
+insert into test (id, name, birthday, email) values (21, 'Mufi', '7/26/1926', 'mnuzztik@mozilla.org');
+insert into test (id, name, birthday, email) values (22, 'Dean', '9/22/1921', 'dpikel@sbwire.com');
+insert into test (id, name, birthday, email) values (23, 'Suzie', '8/12/1949', 'sjanjusevicm@cloudflare.com');
+insert into test (id, name, birthday, email) values (24, 'Tressa', '4/4/1987', 'ttattersdilln@wufoo.com');
+insert into test (id, name, birthday, email) values (25, 'Frankie', '7/20/1992', 'fmarieno@reference.com');
+insert into test (id, name, birthday, email) values (26, 'Constantina', '4/17/1931', 'cgledhallp@prweb.com');
+insert into test (id, name, birthday, email) values (27, 'Bernette', '1/10/1986', 'bchristofolq@macromedia.com');
+insert into test (id, name, birthday, email) values (28, 'Nadiya', '5/19/1981', 'nhessayr@webeden.co.uk');
+insert into test (id, name, birthday, email) values (29, 'Brandi', '7/24/2009', 'bimesons@jigsy.com');
+insert into test (id, name, birthday, email) values (30, 'Lawrence', '8/27/1956', 'lbrockbankt@twitter.com');
+insert into test (id, name, birthday, email) values (31, 'Virgilio', '2/4/1980', 'vimpeyu@cafepress.com');
+insert into test (id, name, birthday, email) values (32, 'Arther', '11/24/1910', 'azuanazziv@businessinsider.com');
+insert into test (id, name, birthday, email) values (33, 'Artemus', '8/13/1947', 'asabiew@dmoz.org');
+insert into test (id, name, birthday, email) values (34, 'Jean', '4/10/1995', 'jcoughx@seesaa.net');
+insert into test (id, name, birthday, email) values (35, 'Balduin', '1/9/1937', 'bpopplestony@youku.com');
+insert into test (id, name, birthday, email) values (36, 'Jeane', '4/14/1903', 'jcattowz@ucoz.com');
+insert into test (id, name, birthday, email) values (37, 'Red', '3/30/1893', 'rhalsted10@about.com');
+insert into test (id, name, birthday, email) values (38, 'Linnea', '6/9/1902', 'lsotworth11@constantcontact.com');
+insert into test (id, name, birthday, email) values (39, 'Padraig', '2/14/1922', 'pbeernt12@w3.org');
+insert into test (id, name, birthday, email) values (40, 'Yanaton', '5/10/1977', 'ybartell13@free.fr');
+insert into test (id, name, birthday, email) values (41, 'Lilia', '7/12/1935', 'lbagster14@umn.edu');
+insert into test (id, name, birthday, email) values (42, 'Beverly', '9/25/2016', 'bblackaby15@goo.gl');
+insert into test (id, name, birthday, email) values (43, 'Joye', '9/5/1949', 'jstannion16@skype.com');
+insert into test (id, name, birthday, email) values (44, 'Katrina', '1/14/1904', 'kavrahm17@shutterfly.com');
+insert into test (id, name, birthday, email) values (45, 'Nathanael', '7/12/1936', 'nmorgue18@cpanel.net');
+insert into test (id, name, birthday, email) values (46, 'Alfons', '9/9/1949', 'abiers19@thetimes.co.uk');
+insert into test (id, name, birthday, email) values (47, 'Mack', '8/22/1927', 'mtuxwell1a@furl.net');
+insert into test (id, name, birthday, email) values (48, 'Maure', '3/11/2000', 'madacot1b@reddit.com');
+insert into test (id, name, birthday, email) values (49, 'Karine', '4/3/2022', 'kbewly1c@domainmarket.com');
+insert into test (id, name, birthday, email) values (50, 'Irwin', '6/8/2021', 'iraubenheimers1d@netvibes.com');
+insert into test (id, name, birthday, email) values (51, 'Meir', '4/17/1941', 'mangeli1e@privacy.gov.au');
+insert into test (id, name, birthday, email) values (52, 'Olva', '8/20/1926', 'odawtrey1f@nyu.edu');
+insert into test (id, name, birthday, email) values (53, 'Corny', '11/28/1982', 'cscourfield1g@taobao.com');
+insert into test (id, name, birthday, email) values (54, 'Aleece', '8/3/1917', 'afynes1h@xing.com');
+insert into test (id, name, birthday, email) values (55, 'Rubi', '2/3/2022', 'rbentick1i@is.gd');
+insert into test (id, name, birthday, email) values (56, 'Jobey', '5/5/1905', 'jhambidge1j@bandcamp.com');
+insert into test (id, name, birthday, email) values (57, 'Nanine', '10/20/2005', 'nbeevis1k@nifty.com');
+insert into test (id, name, birthday, email) values (58, 'Vivyanne', '5/10/1927', 'vjakubowicz1l@prnewswire.com');
+insert into test (id, name, birthday, email) values (59, 'Cherida', '9/30/1904', 'chambright1m@hao123.com');
+insert into test (id, name, birthday, email) values (60, 'Spencer', '1/14/2002', 'sroll1n@1und1.de');
+insert into test (id, name, birthday, email) values (61, 'Roxane', '10/17/1972', 'rmesnard1o@wordpress.com');
+insert into test (id, name, birthday, email) values (62, 'Nils', '3/20/1983', 'nrubel1p@yolasite.com');
+insert into test (id, name, birthday, email) values (63, 'Jaimie', '5/3/1921', 'jfarrear1q@godaddy.com');
+insert into test (id, name, birthday, email) values (64, 'Annie', '10/29/1961', 'aallred1r@mayoclinic.com');
+insert into test (id, name, birthday, email) values (65, 'Timmy', '9/28/1925', 'tgravestone1s@myspace.com');
+insert into test (id, name, birthday, email) values (66, 'Dante', '7/11/1921', 'dweatherburn1t@samsung.com');
+insert into test (id, name, birthday, email) values (67, 'Hort', '9/24/1992', 'hscardefield1u@umich.edu');
+insert into test (id, name, birthday, email) values (68, 'Ronnie', '8/9/2000', 'rmacgoun1v@tinypic.com');
+insert into test (id, name, birthday, email) values (69, 'Beatrix', '8/6/1991', 'bscholte1w@ebay.co.uk');
+insert into test (id, name, birthday, email) values (70, 'Bree', '2/5/1958', 'bheyfield1x@ning.com');
+insert into test (id, name, birthday, email) values (71, 'Shena', '7/18/1972', 'sbartelet1y@google.co.jp');
+insert into test (id, name, birthday, email) values (72, 'Rudolfo', '2/2/2022', 'rbergstrand1z@census.gov');
+insert into test (id, name, birthday, email) values (73, 'Luisa', '6/24/1979', 'lbibb20@pagesperso-orange.fr');
+insert into test (id, name, birthday, email) values (74, 'Sheila', '4/1/2011', 'spasterfield21@nps.gov');
+insert into test (id, name, birthday, email) values (75, 'Judith', '7/12/1950', 'jkobera22@yelp.com');
+insert into test (id, name, birthday, email) values (76, 'Gibby', '5/6/1986', 'ghedylstone23@so-net.ne.jp');
+insert into test (id, name, birthday, email) values (77, 'Cathe', '3/2/1906', 'cbaxster24@unc.edu');
+insert into test (id, name, birthday, email) values (78, 'Harv', '7/26/1923', 'hhobgen25@de.vu');
+insert into test (id, name, birthday, email) values (79, 'Dew', '10/5/1911', 'dmaccleod26@bigcartel.com');
+insert into test (id, name, birthday, email) values (80, 'Maudie', '3/21/1965', 'msommerland27@photobucket.com');
+insert into test (id, name, birthday, email) values (81, 'Ogdan', '8/26/1972', 'oinkles28@list-manage.com');
+insert into test (id, name, birthday, email) values (82, 'Collette', '7/19/1912', 'csteinor29@trellian.com');
+insert into test (id, name, birthday, email) values (83, 'Dominick', '1/4/1937', 'dnellen2a@dion.ne.jp');
+insert into test (id, name, birthday, email) values (84, 'Maryanne', '5/9/1989', 'mtantum2b@hao123.com');
+insert into test (id, name, birthday, email) values (85, 'Dex', '4/7/2022', 'dliddington2c@dropbox.com');
+insert into test (id, name, birthday, email) values (86, 'Jessika', '8/9/1960', 'jdegoix2d@go.com');
+insert into test (id, name, birthday, email) values (87, 'Denise', '3/30/1927', 'dbilbey2e@wikia.com');
+insert into test (id, name, birthday, email) values (88, 'Cornie', '7/1/1902', 'ccruikshank2f@rakuten.co.jp');
+insert into test (id, name, birthday, email) values (89, 'Reinwald', '4/17/1921', 'rlopes2g@163.com');
+insert into test (id, name, birthday, email) values (90, 'Jelene', '1/26/1921', 'jlob2h@bbb.org');
+insert into test (id, name, birthday, email) values (91, 'Jorrie', '5/18/2002', 'jsivell2i@biblegateway.com');
+insert into test (id, name, birthday, email) values (92, 'Ceciley', '10/8/1999', 'cbratton2j@spotify.com');
+insert into test (id, name, birthday, email) values (93, 'Barbara', '8/28/1919', 'blathan2k@vistaprint.com');
+insert into test (id, name, birthday, email) values (94, 'Roderigo', '10/16/2003', 'rcathrae2l@ning.com');
+insert into test (id, name, birthday, email) values (95, 'Christabella', '10/22/1997', 'cgrocutt2m@accuweather.com');
+insert into test (id, name, birthday, email) values (96, 'Pamela', '3/1/1922', 'pfendlen2n@blinklist.com');
+insert into test (id, name, birthday, email) values (97, 'Isaac', '11/18/1916', 'idyster2o@geocities.jp');
+insert into test (id, name, birthday, email) values (98, 'Barrett', '6/8/1934', 'bbending2p@illinois.edu');
+insert into test (id, name, birthday, email) values (99, 'Rufe', '11/23/1890', 'rvinker2q@godaddy.com');
+insert into test (id, name, birthday, email) values (100, 'Dyna', '7/15/1990', 'drixon2r@businesswire.com');
+
+3-Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+UPDATE test
+    SET name='Merve ipekci',
+        birthday='1967-10-18',
+        email='mmmm@mm.com'
+WHERE id=1
+
+UPDATE test
+    SET name='esra',
+        birthday='1999-01-01',
+        email='assadsad@asdasd.com'
+WHERE id=2
+
+UPDATE test
+    SET name='aadddddd',
+        birthday='1994-01-01',
+        email='ssssss@asdasd.com'
+WHERE id=3
+
+UPDATE test
+    SET name='xxxxx',
+        birthday='1972-08-27',
+        email='elif@hobidunya.com'
+WHERE id=4
+
+UPDATE test
+    SET name='Mdddd',
+        birthday='1972-03-20',
+        email='ssssa@ddd.com'
+WHERE id=5
+
+
+4-Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
+DELETE FROM test
+WHERE id IN (1,2,3,4,5)
+RETURNING *;
+
+
+
